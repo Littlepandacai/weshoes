2cf029ec2ee1d82786e959815a7f5480f0d340e8
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2023-12-04 23:40:33 +0100

    raderade komentarerna

--- projekt/logic/bojanaFilter/bojanaFilter.js
+++ projekt/logic/bojanaFilter/bojanaFilter.js
@@ -4,18 +4,13 @@
 
 function filterBojana(unfilteredShoeArray, x, y) {
 
-    // Loopa igenom varje sko i den ofiltrerade arrayen
     let filterPriceArray = [];
     for (let shoe of unfilteredShoeArray) {
-        // Kontrollera om skons pris är inom det angivna intervallet [x, y]
         if (shoe.price >= x && shoe.price <= y) {
-            // Om ja, lägg till skon i den filtrerade arrayen
             filterPriceArray.push(shoe);
         }
     }
-    // Returnera den filtrerade arrayen
     return filterPriceArray;
 }
 
-// Anropa funktionen med skorna och prisintervallet [200, 800]
 filterBojana(SHOES, 200, 800);
