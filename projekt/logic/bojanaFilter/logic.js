7637caa16b173253de8955011f61d358b8c9b559
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2023-12-06 14:45:14 +0100

    kommenterade koden med förklaring av Eriks kod, för att hjälpa att förstå

--- projekt/logic/bojanaFilter/logic.js
+++ projekt/logic/bojanaFilter/logic.js
@@ -23,8 +23,11 @@ function filterIfTrue(shoe) {
 
 
 
+//_________________________________________________________________
 
-// Eriks kod inlämningsuppgift filter.js
+// Eriks kod inlämningsuppgift filter.js - vi radera dettna kod när vi är klara med filterfunktionen, den är bara som hjälp.
+
+//________________________________________________________________
 
 // Funktionerna filter_by_level, filter_by_subject och filter_by_language
 // gör i princip exakt samma sak. Se nedan.
@@ -37,126 +40,74 @@ function filterIfTrue(shoe) {
 //    
 
 
+function general_filter(programmes, filter_kind, attribute_id) {
+    // Hämta alla markerade filter för det angivna filterkind (t.ex. "city_filters")
+    const checked_filter_doms = document.querySelectorAll(`#${filter_kind}_filters .checked .text`);
+
+    // Extrahera namnen på de markerade filterna och spara dem i en array
+    const checked_filter_names = array_map(checked_filter_doms, function (dom) { return dom.textContent });
+
+    // Hitta objekten för de markerade filterna
+    const checked_filter_objects = array_map(checked_filter_names, function (filter_name) {
+        return array_find(FILTERS[filter_kind], function (filter) {
+            return filter.name === filter_name;
+        });
+    });
+
+    // Extrahera ID:n för de markerade filterna
+    const checked_filter_ids = array_map(checked_filter_objects, function (filter_object) {
+        return filter_object.id;
+    });
+
+    // Filtrera programmen baserat på de markerade filterna och det angivna attributet
+    programmes = array_filter(programmes, function (programme) {
+        return checked_filter_ids.includes(programme[attribute_id]);
+    });
+
+    // Returnera de filtrerade programmen
+    return programmes;
+}
+
 function filter_programmes() {
     let selected_programmes = PROGRAMMES;
 
+    // Om staden är markerad, filtrera programmen baserat på staden
     if (any_filter_checked("city")) {
-        selected_programmes = filter_by_city(selected_programmes);
+        selected_programmes = general_filter(selected_programmes, "city", "universityID");
     }
 
+    // Om nivån är markerad, filtrera programmen baserat på nivån
     if (any_filter_checked("level")) {
-        selected_programmes = filter_by_level(selected_programmes);
+        selected_programmes = general_filter(selected_programmes, "level", "levelID");
     }
 
+    // Om ämnet är markerat, filtrera programmen baserat på ämnet
     if (any_filter_checked("subject")) {
-        selected_programmes = filter_by_subject(selected_programmes);
+        selected_programmes = general_filter(selected_programmes, "subject", "subjectID");
     }
 
+    // Om språket är markerat, filtrera programmen baserat på språket
     if (any_filter_checked("language")) {
-        selected_programmes = filter_by_language(selected_programmes);
+        selected_programmes = general_filter(selected_programmes, "language", "languageID");
     }
 
+    // Returnera de slutliga filtrerade programmen
     return selected_programmes;
 }
 
-function any_filter_checked(filter_kind) {
-    const checked_filters = document.querySelectorAll(`#${filter_kind}_filters .checked`);
-    return checked_filters.length > 0;
-}
+// Här är förklaringarna för varje del:
 
-function filter_by_city(programmes) {
-    const checked_city_doms = document.querySelectorAll("#city_filters .checked .text");
+//     general_filter-funktionen:
+//         Input: programmes (en array av program), filter_kind (vilken typ av filter vi vill applicera), attribute_id (vilket attribut i 
+//         programobjekten vi vill filtrera på). Funktionen använder document.querySelectorAll för att hämta alla markerade filter 
+//         (de med klassen .checked .text) för det angivna filterkindet. Genom att använda hjälpfunktionen array_map extraherar den namnen på 
+//         de markerade filtren och sparar dem i checked_filter_names-arrayen. Sedan används array_map igen för att hitta objekten för de 
+//         markerade filtren genom att jämföra namnen med filterobjekten som finns i FILTERS[filter_kind]. ID:na för de markerade filtren 
+//         extraheras och lagras i checked_filter_ids. Slutligen filtreras programmen baserat på de markerade filtren och det angivna attributet 
+//         (attribute_id).
 
-    const checked_city_names = array_map(checked_city_doms, function (dom) { return dom.textContent });
-
-    const checked_level_objects = array_map(checked_city_names, function (city_name) {
-        return array_find(CITIES, function (city) {
-            return city.name === city_name;
-        });
-    });
-
-    const checked_city_ids = array_map(checked_level_objects, function (city_object) {
-        return city_object.id;
-    });
-
-    const university_ids = [];
-    for (let city_id of checked_city_ids) {
-        for (let university of UNIVERSITIES) {
-            if (university.cityID === city_id) {
-                university_ids.push(university.id);
-            }
-        }
-    }
-
-    programmes = array_filter(programmes, function (programme) {
-        return university_ids.includes(programme.universityID);
-    });
-
-    return programmes;
-}
-
-function filter_by_level(programmes) {
-    const checked_level_doms = document.querySelectorAll("#level_filters .checked .text");
-
-    const checked_level_names = array_map(checked_level_doms, function (dom) { return dom.textContent });
-
-    const checked_level_objects = array_map(checked_level_names, function (level_name) {
-        return array_find(LEVELS, function (level) {
-            return level.name === level_name;
-        });
-    });
-
-    const checked_level_ids = array_map(checked_level_objects, function (level_object) {
-        return level_object.id;
-    });
-
-    programmes = array_filter(programmes, function (programme) {
-        return checked_level_ids.includes(programme.levelID);
-    });
-
-    return programmes;
-}
-
-function filter_by_subject(programmes) {
-    const checked_subject_doms = document.querySelectorAll("#subject_filters .checked .text");
-
-    const checked_subject_names = array_map(checked_subject_doms, function (dom) { return dom.textContent });
-
-    const checked_subject_objects = array_map(checked_subject_names, function (subject_name) {
-        return array_find(SUBJECTS, function (subject) {
-            return subject.name === subject_name;
-        });
-    });
-
-    const checked_subject_ids = array_map(checked_subject_objects, function (subject_object) {
-        return subject_object.id;
-    });
-
-    programmes = array_filter(programmes, function (programme) {
-        return checked_subject_ids.includes(programme.subjectID);
-    });
-
-    return programmes;
-}
-
-function filter_by_language(programmes) {
-    const checked_language_doms = document.querySelectorAll("#language_filters .checked .text");
-
-    const checked_language_names = array_map(checked_language_doms, function (dom) { return dom.textContent });
-
-    const checked_language_objects = array_map(checked_language_names, function (language_name) {
-        return array_find(LANGUAGES, function (language) {
-            return language.name === language_name;
-        });
-    });
-
-    const checked_language_ids = array_map(checked_language_objects, function (language_object) {
-        return language_object.id;
-    });
-
-    programmes = array_filter(programmes, function (programme) {
-        return checked_language_ids.includes(programme.languageID);
-    });
-
-    return programmes;
-}
+//     filter_programmes-funktionen:
+//         Funktionen skapar en kopia av PROGRAMMES som heter selected_programmes.
+//         För varje typ av filter (stad, nivå, ämne, språk) kontrollerar den om några filter är markerade (any_filter_checked). Om de är det, 
+//         anropas general_filter-funktionen för att filtrera programmen baserat på det specifika filtret och attributet.
+//         Till sist returnerar den de slutliga filtrerade programmen.
