b356f178c294126c6fb857bc4a6502f813a3d000
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2023-12-08 09:42:39 +0100

    Update logic.js

--- projekt/logic/bojanaFilter/logic.js
+++ projekt/logic/bojanaFilter/logic.js
@@ -111,3 +111,65 @@ function filter_programmes() {
 //         För varje typ av filter (stad, nivå, ämne, språk) kontrollerar den om några filter är markerade (any_filter_checked). Om de är det, 
 //         anropas general_filter-funktionen för att filtrera programmen baserat på det specifika filtret och attributet.
 //         Till sist returnerar den de slutliga filtrerade programmen.
+
+
+
+
+//Eriks inlämningsuppgift arrat_functions.js
+
+function array_average(a) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+    return ("" + (sum / a.length)).substring(0, 4);
+}
+
+function array_random_element(a) {
+    const random_index = get_random_number(a.length);
+    return a[random_index];
+}
+
+function array_find(a, test_function) {
+
+    for (let i = 0; i < a.length; i++) {
+        if (test_function(a[i]) === true)
+            return a[i];
+    }
+}
+// Loopar igenom arrayen a och anropar funktionen test_function med varje element i a som argument.
+// Loopen (och funktionen) avbryts när ett test_function returnerar true.
+// Funktionen returnerar elementet i a för vilken test_function returnerade true.
+
+
+function array_filter(a, test_function) {
+
+    let returnArray = [];
+    for (let i = 0; i < a.length; i++) {
+        if (test_function(a[i]) === true)
+            returnArray.push(a[i]);
+    }
+    return returnArray;
+}
+// Loopar igenom arrayen a och anropar funktionen test_function med varje element i a som argument.
+// Funktionen returnerar en ny array med alla elementet i a för vilka test_function returnerade true.
+
+
+function array_each(a, callback) {
+
+    for (let i = 0; a.length; i++) {
+        callback(a[i]);
+    }
+}
+// Loopar igenom arrayen a och anropar funktionen callback med varje element i a som argument.
+
+
+function array_map(a, transform_function) {
+    // Loopar igenom arrayen a och anropar funktionen transform_function med varje element i a som argument.
+    // Funktionen returnerar en ny array med returvärdet av varje anrop av transform_function.
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push(transform_function(a[i]));
+    }
+    return result;
+}
