592975271426d8763529bf1c49197823079171e1
Hampus833 <hampuse266@gmail.com>
2023-12-11 23:26:00 +0100

    header struktur och css klar
    skapat struktur för nav och header. Sidebar går att öppna och stänga (vet inte om den behövs). storlekaar går att ändra.

--- projekt/components/header/header.js
+++ projekt/components/header/header.js
@@ -1,25 +1,56 @@
 "use strict";
 
-function render_nav(parent) {
+function render_nav() {
 
-    const div = document.createElement("div");
-    parent.appendChild(div);
-    div.setAttribute("id", "navigation");
-
-    const container = document.querySelector("#navigation");
+    const container = document.querySelector("nav");
     container.innerHTML = `
-    <div id="menu"></div>
-    <h1>WeShoes</h1>
-    <input type="search" id="search_bar"></input>
-    <div id="shopping_cart"></div>
+    <div id="sidebar"></div>
+    <div id="navigation">
+        <div id="menu">
+            <div id="menu_icon">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+        </div>
+        <h1>WeShoes</h1>
+        <div id="right">
+            <input type="search" id="search_bar"></input>
+            <div id="shopping_cart"></div>
+        </div>
+    </div>
     `;
 
     return {
         menu: container.querySelector("#menu"),
         logo: container.querySelector("h1"),
+        right: container.querySelector("#right"),
         search_bar: container.querySelector("#search_bar"),
         shopping_cart: container.querySelector("#shopping_cart"),
     }
 }
 
-//render_nav(document.querySelector("nav"))
+// let sidebar = document.querySelector("#menu");
+// sidebar.addEventListener("click", function () {
+//     document.querySelector("#sidebar").classList.toggle("active")
+// }
+
+function render_header() {
+
+    const header = document.querySelector("header");
+    header.innerHTML = `
+    <div id="left"></div>
+    <div id="title">
+        <h1>SHOES/<span>ALL</span></h1>
+        <div id="line"></div>
+    </div>
+    `;
+
+    return {
+        left: header.querySelector("#left"),
+        title_box: header.querySelector("#title"),
+        title: header.querySelector("h1"),
+        line: header.querySelector("#line"),
+    }
+}
+
--- projekt/index.js
+++ projekt/index.js
@@ -1,3 +1,10 @@
-let structure_containers = structure_Esmir();
+structure_Esmir();
 
-render_nav(structure_containers.nav);
+render_nav();
+
+let sidebar = document.querySelector("#menu");
+sidebar.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("active")
+})
+
+render_header();
