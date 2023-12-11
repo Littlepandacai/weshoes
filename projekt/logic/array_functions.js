f5479efb3a5ef3e6760995df986cbb2e4b08564f
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2023-12-11 20:04:40 +0100

    gjorde små ändringar i koden

--- projekt/logic/array_functions.js
+++ projekt/logic/array_functions.js
@@ -1,9 +1,39 @@
+
+"use strict";
+
 //Array average (för betygen)
+
+function arrayAverage(a) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+    return ("" + (sum / a.length)).substring(0, 3);
+}
+
+
+function arrayRandomElement(a) {
+    const randomIndex = getRandomNumber(a.length);
+    return a[randomIndex];
+}
+
+
 //Array find, returnerar det första elementet
 
-//arrayEach, går igenom en array och anropar callback function för varje element
+function arrayFind(array, findFunction) {
+    for (let element of array) {
+        let resultat = findFunction(element);
+        if (resultat === true) {
+            return element;
+        }
+    }
+}
 
-function arrayFilter (array, callback) {
+
+
+//arrayFilter, testar maxPrice och returnerar boleskt värde (true/false)
+
+function arrayFilter(array, callback) {
 
     let temp = [];
     for (let shoe of array) {
@@ -15,33 +45,28 @@ function arrayFilter (array, callback) {
 }
 
 
-//arrayFilter, testar maxPrice och returnerar boleskt värde (true/false)
 
+//arrayEach, går igenom en array och anropar callback function för varje element
 
-function arrayFind (array, findFunction) 
-{
-  for (let element of array) {
-    let resultat = findFunction(element);
-    if (resultat === true) {
-        return element;
-    }
-  }
-}
-
-
-function arrayAverage(a) {
-    let sum = 0;
-    for (let i = 0; i < a.length; i++) {
-        sum += a[i];
-    }
-    return ("" + (sum / a.length)).substring(0, 4);
-}
-
-function arrayEach (array, callback) {
+function arrayEach(array, callback) {
     for (let i = 0; i < array.length; i++) {
         callback(array[i]);
     }
 }
+
+//arrayMap
+
+function arrayMap(a, transformFunction) {
+
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push(transformFunction(a[i]));
+    }
+    return result;
+}
+
+
+
 
 
 console.log(arrayFilter(SHOES, function (x) {
