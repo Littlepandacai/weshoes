f8c5ff57d07af1f3e06faca9287b275ae5c77b97
Hampus833 <hampuse266@gmail.com>
2023-12-12 21:59:56 +0100

    fixar för merge

--- projekt/index.js
+++ projekt/index.js
@@ -1,10 +1,3 @@
 structure_Esmir();
 
-render_nav();
 
-let sidebar = document.querySelector("#menu");
-sidebar.addEventListener("click", function () {
-    document.querySelector("#sidebar").classList.toggle("active")
-})
-
-render_header();
