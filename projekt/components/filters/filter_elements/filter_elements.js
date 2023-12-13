c8ec817d7cf4cc1fc15f94e6c694e095a7c4b403
whoisluci <lucmov99@gmail.com>
2023-12-13 09:40:13 +0100

    Fixed checkmark issues

--- projekt/components/filters/filter_elements/filter_elements.js
+++ projekt/components/filters/filter_elements/filter_elements.js
@@ -10,11 +10,13 @@ function render_filter_element (parent, text) {
     <div class="text">${text}</div>
   `;
 
-  container.addEventListener("click", function () 
-  {
-    const checkmark_box = document.querySelector(".checkmark_box");
-    checkmark_box.innerHTML = `<p class="checkmark">X</p>`;
-    container.classList.toggle("checked");    
+  container.addEventListener("click", function () {
+    const checkmark_box = this.querySelector(".checkmark_box");
+    
+    // Toggle mellan "X" och inget innehåll
+    checkmark_box.innerHTML = checkmark_box.innerHTML === "" ? `<p class="checkmark">X</p>` : "";
+  
+    container.classList.toggle("checked");
     // update catalogue
   });
 }
