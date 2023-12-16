d5ae477467985b70760fc3775f028a5c03a43367
Littlepandacai <caiamanda77@gmail.com>
2023-12-16 20:40:03 +0100

    forsättning, en del css

--- projekt/components/catalogue/catalogue.js
+++ projekt/components/catalogue/catalogue.js
@@ -1,27 +1,47 @@
 "use strict";
 
 function catalogue () {
-    const div = document.createElement("div");
-    document.body.rightBottom.appendChild(div);
-    div.setAttribute("id", "catalogue");
 
-    const container = document.querySelector("#catalogue");
+    const container = document.getElementById("rightBottom");
     container.innerHTML = `
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-        <div id="catalogue-object"></div>
-
-  `;
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    <div class="catalogueObject"></div>
+    `;
 
   return {
-    div: container.querySelector("catalogue-object"),
+    catalogueObject: container.querySelectorAll(".catalogueObject")
   };
 }
 
 catalogue();
+
+function render_catalogueObject(){
+
+  const container = document.querySelectorAll(".catalogueObject");
+  container.innerHTML = `
+  <div class="imageBox">
+      <img src="" alt="">
+      <p>TYPE</p>
+  </div>
+  <div class="textBox">
+      <h2>NAME OF SHOE</h2>
+      <div>
+        <p>COUNTRY</p>
+        <p>PRICE</p>
+      </div>
+  </div>
+  `;
+  return{
+    imageBox: container.querySelector(".imageBox"),
+    textBox: container.querySelector(".textBox"),
+    // somethingsomething: container.querySelector("h2"),
+    //somethingsomething: container.querySelector("p"),
+  };
+}
