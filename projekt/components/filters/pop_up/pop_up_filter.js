a9205d5fe7334ebf46dedfd21889b0a9a51c012d
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2023-12-16 13:09:36 +0100

    Update pop_up_filter.js

--- projekt/components/filters/pop_up/pop_up_filter.js
+++ projekt/components/filters/pop_up/pop_up_filter.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function renderPopUpFilter(parent) {
-    const container = document.createElement("div");
-    container.id = "popupContainer";
-    parent.append(container);
+  const container = document.createElement("div");
+  container.id = "popupContainer";
+  parent.append(container);
 
-    container.innerHTML = `
+  container.innerHTML = `
       <button id="openPopupBtn">Click Me</button>
   
       <div class="popup" id="popup">
@@ -16,25 +16,25 @@ function renderPopUpFilter(parent) {
       </div>
     `;
 
-    const openPopupBtn = document.getElementById('openPopupBtn');
-    const closePopupBtn = document.getElementById('closePopupBtn');
-    const popup = document.getElementById('popup');
-    const popupText = document.getElementById('popupText');
+  const openPopupBtn = document.getElementById('openPopupBtn');
+  const closePopupBtn = document.getElementById('closePopupBtn');
+  const popup = document.getElementById('popup');
+  const popupText = document.getElementById('popupText');
 
-    openPopupBtn.addEventListener('click', function () {
-        popup.style.display = 'block';
-        popupText.innerHTML = 'WeShoes';
-    });
+  openPopupBtn.addEventListener('click', function () {
+    popup.style.display = 'block';
+    popupText.innerHTML = 'WeShoes';
+  });
 
-    closePopupBtn.addEventListener('click', function () {
-        popup.style.display = 'none';
-    });
+  closePopupBtn.addEventListener('click', function () {
+    popup.style.display = 'none';
+  });
 
-    window.addEventListener('click', function (event) {
-        if (event.target === popup) {
-            popup.style.display = 'none';
-        }
-    });
+  // window.addEventListener('click', function (event) {
+  //     if (event.target === popup) {
+  //         popup.style.display = 'none';
+  //     }
+  // });
 }
 
 
