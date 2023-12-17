6385fc56964c9e911452e2e69b0a988482b64966
whoisluci <lucmov99@gmail.com>
2023-12-17 17:46:27 +0100

    Added JS-file for carousel button

---
+++ projekt/components/carousel/carousel_button.js
@@ -0,0 +1,9 @@
+let body = document.querySelector("body");
+renderCarousel(body);
+
+let button = document.querySelector("#circle");
+button.innerHTML = `
+<a href="#middle">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Down_arrowhead.svg/1280px-Down_arrowhead.svg.png">
+</a>
+`;
