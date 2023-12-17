396811d5aae74987d5f1ac75e7637ca00466f295
whoisluci <lucmov99@gmail.com>
2023-12-17 20:24:07 +0100

    Updated price filter to include eventListener

--- projekt/components/filters/price_filter/price_filter.js
+++ projekt/components/filters/price_filter/price_filter.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function renderPriceFilter (parent) {
     const container = document.createElement("div");
     container.id = "price_filter";
@@ -11,6 +13,20 @@ function renderPriceFilter (parent) {
     <input id="max_price_input">
     <p id="amount">AMOUNT OF ARTICLES</p>
     `;
-  }
 
+    let inputMinPrice = document.querySelector("#min_price_input");
+    let inputMaxPrice = document.querySelector("#max_price_input");
+    let max_min_price = [inputMinPrice, inputMaxPrice];
+    let price_filter;
+
+    max_min_price.forEach(function (elem) {
+        elem.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                console.log(price_minmax_filter(SHOES, parseInt(inputMinPrice.value), parseInt(inputMaxPrice.value)));
+                price_filter = price_minmax_filter(SHOES, parseInt(inputMinPrice.value), parseInt(inputMaxPrice.value))
+            }
+        })
+        return price_filter
+  });
+}
   
