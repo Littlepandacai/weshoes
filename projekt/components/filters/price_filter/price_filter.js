38d2bbf08e06131ffc6f06a9fde6abfc2065aa13
RmebbMach <94225083+RmebbMach@users.noreply.github.com>
2023-12-18 00:03:43 +0100

    created amount_products function

--- projekt/components/filters/price_filter/price_filter.js
+++ projekt/components/filters/price_filter/price_filter.js
@@ -1,10 +1,10 @@
 "use strict";
 
-function renderPriceFilter (parent) {
+function renderPriceFilter(parent) {
     const container = document.createElement("div");
     container.id = "price_filter";
     parent.append(container);
-  
+
     container.innerHTML = `
     <h3>PRICE</h3>
     <p id="min_price_text">MIN PRICE</p>
@@ -12,6 +12,7 @@ function renderPriceFilter (parent) {
     <p id="max_price_text">MAX PRICE</p>
     <input id="max_price_input">
     <p id="amount">AMOUNT OF ARTICLES</p>
+    <p id="result"></p>
     `;
 
     let inputMinPrice = document.querySelector("#min_price_input");
@@ -22,11 +23,22 @@ function renderPriceFilter (parent) {
     max_min_price.forEach(function (elem) {
         elem.addEventListener("keypress", function (event) {
             if (event.key === "Enter") {
+
                 console.log(price_minmax_filter(SHOES, parseInt(inputMinPrice.value), parseInt(inputMaxPrice.value)));
                 price_filter = price_minmax_filter(SHOES, parseInt(inputMinPrice.value), parseInt(inputMaxPrice.value))
+                amount_products(price_filter.length)
             }
         })
         return price_filter
-  });
+    });
 }
-  
+
+let amount_div = document.createElement("div")
+
+function amount_products(amount) {
+    let container = document.querySelector("#amount");
+    let result = document.querySelector("#result")
+    container.appendChild(result);
+    result.innerHTML = `<p>${amount} products</p>`
+    return amount
+}
