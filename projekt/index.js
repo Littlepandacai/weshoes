ec13f16be0d826dbb8498abfc6af163a06c299dd
whoisluci <lucmov99@gmail.com>
2023-12-17 17:43:46 +0100

    Updated index files

--- projekt/index.js
+++ projekt/index.js
@@ -1,4 +1,43 @@
 let parent = document.body;
 renderButton(parent);
 
+<<<<<<< Updated upstream
 renderStructure (parent)
+=======
+// Med detta anrop skapar vi HTML-strukturen.
+// Returvärdet ger oss referenser till viktiga HTML-element.
+const structureContainers = renderStructure(body);
+
+// Använd returvärdet av render_structure(), där finns referenser till alla HTML-element (parents)
+// som behövs för att rendera komponenterna:
+// - header
+// - kindsFilters
+// - countriesFilters
+// - priceFilters
+// - sorterList
+// - catalogueList
+// - popUp
+// - aboutUs
+
+// Så här renderas header med hjälp av returvärdet av render_structure:
+
+//  OBS!!! inte den korrekta renderingen, måste korrigeras innan!!
+
+render_header(body);
+render_nav(body);
+renderCountryFilter(left);
+renderKindFilter (left);
+renderPriceFilter (left);
+//renderPopUpFilter(parentElement);
+
+
+
+// Här ska resten av render_funktionerna anropas (listan av komponenter finns ovan).
+
+//funktion för att få sidebar att funka (om vi behöver en sidebar)
+let sidebar_menu = document.querySelector("#menu");
+sidebar_menu.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("active")
+})
+
+>>>>>>> Stashed changes
