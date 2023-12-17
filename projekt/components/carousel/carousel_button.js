771ab179bfebc38e73cb301cd97734ccc3856ff5
RmebbMach <94225083+RmebbMach@users.noreply.github.com>
2023-12-17 22:06:54 +0100

    add link to arrow button on landing page

--- projekt/components/carousel/carousel_button.js
+++ projekt/components/carousel/carousel_button.js
@@ -4,6 +4,6 @@ render_carousel(body);
 let button = document.querySelector("#circle");
 button.innerHTML = `
 <a href="#middle">
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Down_arrowhead.svg/1280px-Down_arrowhead.svg.png">
+  <a href="#wrapper"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Down_arrowhead.svg/1280px-Down_arrowhead.svg.png"><a>
 </a>
 `;
