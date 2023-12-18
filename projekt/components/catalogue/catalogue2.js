216fc5ff17865e21c2bbeb17622655c5768cf3b4
RmebbMach <94225083+RmebbMach@users.noreply.github.com>
2023-12-18 09:21:48 +0100

    add price on each products

--- projekt/components/catalogue/catalogue2.js
+++ projekt/components/catalogue/catalogue2.js
@@ -1,25 +1,26 @@
 "use strict";
 
 function renderCatalogue(parent, shoes) {
-    for (let i = 0; i < shoes.length; i++) {
-      const container = document.createElement("div");
-      container.classList.add("catalogue");
-      container.id = shoes[i].id;
-      parent.append(container);
-  
-      const country = array_find(COUNTRIES, (x) => x.id === shoes[i].country_id);
-      const type = array_find(KINDS, (x) => x.id === shoes[i].kind_id);
-  
-      container.innerHTML = `
+  for (let i = 0; i < shoes.length; i++) {
+    const container = document.createElement("div");
+    container.classList.add("catalogue");
+    container.id = shoes[i].id;
+    parent.append(container);
+
+    const country = array_find(COUNTRIES, (x) => x.id === shoes[i].country_id);
+    const type = array_find(KINDS, (x) => x.id === shoes[i].kind_id);
+    const price = array_find(SHOES, (x) => x.price === shoes[i].price)
+
+    container.innerHTML = `
         <div>
           <h1>${shoes[i].name}</h1>
           <div class="country">${country.name}</div>
           <div class="type">${type.name}</div>
+          <div class="type">${price.price}sek</div>
         </div>
       `;
-  
-      const image_path = shoes[i].file_name;
-      container.style.backgroundImage = `url(./images/skobilder/${image_path})`;
-      console.log("loop");
-    }
+
+    const image_path = shoes[i].file_name;
+    container.style.backgroundImage = `url(./images/skobilder/${image_path})`;
   }
+}
