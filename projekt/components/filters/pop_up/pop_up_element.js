1ae6aafd487901414bef229d8683265a84638fb4
whoisluci <lucmov99@gmail.com>
2023-12-20 16:47:14 +0100

    Updated pop_up_element folder

--- projekt/components/filters/pop_up/pop_up_element.js
+++ projekt/components/filters/pop_up/pop_up_element.js
@@ -2,7 +2,6 @@
 
 function renderPopUpElement() {
     let shoeImg = document.querySelectorAll(".catalogue > div:first-child");
-
     let wrapper = document.querySelector("#wrapper");
 
     for (let shoe of shoeImg) {
@@ -52,33 +51,7 @@ function renderPopUpElement() {
       </div>
         `;
 
-
-            const closePopupBtn = document.getElementById('closePopupBtn');
-            const popup = document.getElementById('popup');
-
-
-            openPopupBtn.addEventListener('click', function () {
-                popup.style.display = 'block';
-
-            });
-
-            closePopupBtn.addEventListener('click', function () {
-                popup.style.display = 'none';
-            });
-
-            // window.addEventListener('click', function (event) {
-            //     if (event.target === popup) {
-            //         popup.style.display = 'none';
-            //     }
-            // });
-
         });
     }
-
-
-
-    // const parentElement = document.body;
-    // renderPopUpFilter(parentElement);
-
 }
 
