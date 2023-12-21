8310872b0c525d32384b1c8e3f73b6402fde8528
whoisluci <lucmov99@gmail.com>
2023-12-21 14:43:28 +0100

    Linked reviews to pop-up component

--- projekt/components/filters/pop_up/pop_up_element.js
+++ projekt/components/filters/pop_up/pop_up_element.js
@@ -11,74 +11,71 @@ function renderPopUpElement() {
             const container = document.createElement("div");
             container.id = "popupContainer";
             main.append(container);
-            for (let product of SHOES) {
-
-                let reviewArray = array_filter(REVIEWS, function (review) {
-                    return shoe.id === review.shoe_id;
-                });
-
-
-                for (let review of reviewArray) {
-                    let container = document.querySelector("#reviewsText");
-                    let reviewDiv = document.createElement("div");
-                    reviewDiv.classList.add("reviewDiv");
-                    container.appendChild(reviewDiv);
-
-                    reviewDiv.innerHTML = `
-                        <h1 id="score">${review.score}</h1>
-                        <p id="revText">${review.rev}</p>      
-                    `;
-
-                    if (review.rev === "") {
-                        let revText = document.querySelector("#revText");
-                        revText.textContent = "No review text.";
-
-                    }
-                }
-            }
-
-
-
 
             container.innerHTML = `
     
-      <div class="popup" id="popup">
-            <span class="close" id="closePopupBtn">&times;</span>
-           
-          <div id="popUpItems">
-              <div id="boxTopLeft">
-                  <div id="kinds">
-                      <h1>Name of Shoe</h1>
-                      <p>COUNTRY</p>
-                      <p>TYPE</p>
-                  </div>
-                  <div id="size">
-                      <div class="sizeType">37</div>
-                      <div class="sizeType">38</div>
-                      <div class="sizeType">39</div>
-                      <div class="sizeType">40</div>
-                      <div class="sizeType">41</div>
-                      <div class="sizeType">42</div>
-                  </div>
-                  <div id="reviews">
-                      <p>0/5</p>
-                  </div>
-              </div>
-              <div id="reviewsText">
-                  <p class="content">Reviews, content</p>
-              </div>
-              <div id="boxTopRight">
-                  <div id="image"></div>
-              </div>
-              <div id="boxBottomRight">
-                  <div id="price">
-                      <h3>PRICE</h3>
-                      <button id="buy">BUY</button>
-                  </div>
-              </div>
-          </div>
-      </div>
-        `;
+            <div class="popup" id="popup">
+                <span class="close" id="closePopupBtn">&times;</span>
+                <div id="popUpItems">
+                    <div id="boxTopLeft">
+                        <div id="kinds">
+                            <h1>Name of Shoe</h1>
+                            <p>COUNTRY</p>
+                            <p>TYPE</p>
+                        </div>
+                        <div id="size">
+                            <div class="sizeType">37</div>
+                            <div class="sizeType">38</div>
+                            <div class="sizeType">39</div>
+                            <div class="sizeType">40</div>
+                            <div class="sizeType">41</div>
+                            <div class="sizeType">42</div>
+                        </div>
+                        <div id="reviews">
+                            <p>0/5</p>
+                        </div>
+                    </div>
+                    <div id="reviewsText">
+                        <p class="content">Reviews, content</p>
+                    </div>
+                    <div id="boxTopRight">
+                        <div id="image"></div>
+                    </div>
+                    <div id="boxBottomRight">
+                        <div id="price">
+                            <h3>PRICE</h3>
+                            <button id="buy">BUY</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            let shoeId = Number(shoe.getAttribute("id"));
+            console.log(shoeId);
+            let reviewArray = array_filter(REVIEWS, function (product) {
+             return product.shoe_id === shoeId});
+
+            for (let review of reviewArray) {
+                
+                        let revContainer = document.querySelector("#reviewsText");
+                        let reviewDiv = document.createElement("div");
+                        reviewDiv.classList.add("reviewDiv");
+                        revContainer.appendChild(reviewDiv);
+
+                        reviewDiv.innerHTML = `
+                            <h1 id="score">${review.score}</h1>
+                            <p id="revText">${review.rev}</p>      
+                        `;
+
+                        if (review.rev === "") {
+                            let revText = document.querySelector("#revText");
+                            revText.textContent = "No review text.";
+
+                        }
+                }
+            
+
 
             let closePopUpBtn = document.querySelector("#closePopupBtn")
             closePopUpBtn.addEventListener("click", function () {
@@ -88,4 +85,6 @@ function renderPopUpElement() {
         });
     }
 }
+
+
 
