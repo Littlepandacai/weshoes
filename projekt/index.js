142c5809377c970a25a255fac6cc28c13267408a
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2023-12-23 11:03:09 +0100

    raderade komentarer som var bortkomenterade

--- projekt/index.js
+++ projekt/index.js
@@ -2,22 +2,6 @@
 // Returvärdet ger oss referenser till viktiga HTML-element.
 const structureContainers = renderStructure(body);
 
-
-// Använd returvärdet av render_structure(), där finns referenser till alla HTML-element (parents)
-// som behövs för att rendera komponenterna:
-// - header
-// - kindsFilters
-// - countriesFilters
-// - priceFilters
-// - sorterList
-// - catalogueList
-// - popUp
-// - aboutUs
-
-// Så här renderas header med hjälp av returvärdet av render_structure:
-
-//  OBS!!! inte den korrekta renderingen, måste korrigeras innan!!
-
 render_header(body);
 render_nav(body);
 renderCountryFilter(left);
@@ -30,12 +14,3 @@ renderCatalogueList(rightBottom, SHOES);
 let main = document.querySelector("main");
 renderPopUpElement(main);
 renderFooter(structureContainers.footer);
-
-
-// Här ska resten av render_funktionerna anropas (listan av komponenter finns ovan).
-
-//funktion för att få sidebar att funka (om vi behöver en sidebar)
-// let sidebar_menu = document.querySelector("#menu");
-// sidebar_menu.addEventListener("click", function () {
-//     document.querySelector("#sidebar").classList.toggle("active")
-// })
